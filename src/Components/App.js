14e3d548bc37d688fe76d8b65f07a40b24eba03d
alexandre <alexandrehlandgraf@gmail.com>
2021-03-26 17:20:32 -0300

    adding bootstrap

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,42 +1,68 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react';
+import Recipe from './Recipes/Recipe';
 
 function App() {
   const [recipes, setRecipes] = useState([]);
-  
+
   useEffect(() => 
   {
     const getRecipes = async () => 
     {
       await fetch('https://localhost:5001/api/recipe')
-        .then(async (response) => 
+      .then(async (response) => 
+      {
+        if(response.ok)
         {
-          if(response.ok)
-          {
-            let data = await response.json();
-            console.log(data);
-            setRecipes([...data]);
-          }
-        });
+          let data = await response.json();
+          console.log(data);
+          setRecipes([...data]);
+        }
+      });
     };
 
     getRecipes();
   }, [])
 
+  const handleSubmit = (event) => 
+  {
+    event.preventDefault();
+    console.log("teste");
+  }
+
   return (
     <>
-    <div>
-      {recipes.map(recipe => (
-        <div> 
-          <h5>{recipe.name}</h5>
-          <h6>Ingredientes: </h6>
-          <ul>
-            {recipe.ingredients.map(ingredient => (
-              <li> {ingredient.name}</li>
-            ))}
-          </ul>
+      <div className="container-fluid p-5">
+        <div className="row justify-content-center">
+          <div className="col-4">
+            <form onSubmit={handleSubmit}>
+              <div className="form-group mb-3">
+                <span>Nome: </span>
+                <input type="text" class="form-control" />
+              </div>
+              <div className="form-group row mb-3">
+                <div class="form-group col-4">
+                  <span>Ingrediente: </span>
+                  <input type="text" class="form-control" />
+                </div>
+                <div class="form-group col-4">
+                  <span>Quantidade: </span>
+                  <input type="number" class="form-control" />
+                </div>
+                <div class="form-group col-4">
+                  <span>Unidade: </span>
+                  <input type="text" class="form-control" />
+                </div>
+              </div>
+              <div className="form-group row justify-content-end p-2">
+                <button type="submit" className="btn btn-success">Adicionar</button>
+              </div>
+            </form>
+          </div>
         </div>
-      ))}
-    </div>
+      </div>
+      <div className="container-fluid p-5">
+          <Recipe recipes={recipes}/>
+      </div>
     </>
   );
 }
